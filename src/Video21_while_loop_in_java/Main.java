eca9d9e4c0c67f425f44042f6330907c518e9538
Ibrahim Abiola <ibrahimabiola75@yahoo.com>
2026-05-16 16:40:06 +0100

    while loop in Java

--- src/Video21_while_loop_in_java/Main.java
+++ src/Video21_while_loop_in_java/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main (String[] args){
 
+        // EXAMPLE 1
+
         // while loop in Java;
 
 
@@ -23,16 +25,54 @@ public class Main {
 //
 //        scanner.close();
 
+        // EXAMPLE 2
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String response = "";
+//
+//        while(!response.equals("Q")){
+//            System.out.println("You are playing a game");
+//            System.out.print("Press Q to quit: ");
+//            response = scanner.next().toUpperCase();
+//        }
+//        System.out.println("You quit the game");
+//
+//        scanner.close();
+
+        // EXAMPLE 3
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int age = 0;
+//
+//        System.out.print("Enter your age: ");
+//        age = scanner.nextInt();
+//
+//        while(age < 0){
+//            System.out.println("Your age cant be negative number");
+//            System.out.print("Enter your age: ");
+//            age = scanner.nextInt();
+//        }
+//
+//        System.out.println("You are " + age + " years old");
+//
+//        scanner.close();
+
+        // EXAMPLE 4
+
         Scanner scanner = new Scanner(System.in);
 
-        String response = "";
+        int number = 0;
 
-        while(!response.equals("Q")){
-            System.out.println("You are playing a game");
-            System.out.print("Press Q to quit: ");
-            response = scanner.next().toUpperCase();
-        }
-        System.out.println("You quit the game");
+        do {
+            System.out.print("Enter a number between 1 to 10: ");
+            number = scanner.nextInt();
+        }while(number < 1 || number > 10);
+
+        System.out.println("You picked " + number);
+
+        scanner.close();
 
 
     }
